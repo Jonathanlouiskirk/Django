e6adf28ea76803b06580e0dc5a3caa56a7b56462
Jonathanlouiskirk <qcpskirk@gmail.com>
2022-04-15 21:31:37 -0400

    comment

--- hellosite/hellosite/settings.py
+++ hellosite/hellosite/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'helloapp.apps.HelloappConfig',
+    'helloapp.apps.HelloappConfig'
+    
+    
 ]
 
 MIDDLEWARE = [
